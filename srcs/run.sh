52550155aff4a7dffd242c75ca5187a489d09f46
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2025-01-01 01:41:58 +0100

    add run script

---
+++ srcs/run.sh
@@ -0,0 +1,9 @@
+#
+
+MARIADB_DIR=/Users/macair/Documents/hossine/data/mariadb
+WORDPRESS_DIR=/Users/macair/Documents/hossinedata/wp
+
+
+
+sudo chown -R $(whoami) $MARIADB_DIR
+chmod -R 755 $MARIADB_DIR
